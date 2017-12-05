b030e3f5b2c71af0c4058d6852c29694ed095ca7
Kite <ixkaito@gmail.com>
2017-12-05 17:39:59 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# KITERETZ WordPress Theme
---
+++ gulp_tasks/browersync.js
@@ -0,0 +1,13 @@
+const browsersync = require('browser-sync').create();
+const config      = require('../kiteretz.config.js');
+const gulp        = require('gulp');
+
+gulp.task('browsersync', function () {
+  return browsersync.init({
+      proxy: config.browsersync.siteurl
+  });
+});
+
+gulp.task('browser-reload', function () {
+  return browsersync.reload();
+});
