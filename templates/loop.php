83f29f0b7595ee1b3f9a019af813651c57884b59
Kite <ixkaito@gmail.com>
2017-12-26 00:44:28 +0900

    Add share button

--- templates/loop.php
+++ templates/loop.php
@@ -7,6 +7,11 @@
 		<article class="entry">
 			<h1 class="entry-title"><?php the_title(); ?></h1>
 			<div class="entry-meta"><?php echo get_the_time( 'Y.m.d' ); ?></div>
+			<div class="entry-share">
+				<div class="fb-like" data-href="<?php the_permalink(); ?>" data-layout="button" data-action="like" data-show-faces="false" data-share="true"></div>
+				<a href="https://twitter.com/share" class="twitter-share-button" data-url="<?php the_permalink(); ?>">Tweet</a>
+				<a data-pocket-label="pocket" data-pocket-count="horizontal" class="pocket-btn" data-lang="en" data-save-url="<?php the_permalink(); ?>"></a>
+			</div>
 			<div class="entry-content">
 				<?php the_content(); ?>
 			</div>
